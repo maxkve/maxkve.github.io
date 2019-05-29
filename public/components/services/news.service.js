d4375672f15b3d50d4a75b4b772465b46d9e310b
Unknown <max.kvelland@gmail.com>
2019-05-29 23:52:00 +0200

    CSS polish, new menu, etc

--- public/components/services/news.service.js
+++ public/components/services/news.service.js
@@ -3,26 +3,26 @@
 angular.module("epgpApp").service("newsService", function() {
   let news = {
     1: {
-      title: "Sanctuary progressing in MC!",
+      title: "We're recruiting for classic!",
       img: "public/assets/moltencore.jpg",
+      date: "2019-05-29",
+      summary: "Interested in joining? Hit the apply-button above!",
       content:
         "Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem"
     },
     2: {
-      title: "Sanctuary progressing in MC!",
-      img: "public/assets/moltencore.jpg",
+      title: "WoW Classic has been announced!",
+      img: "public/assets/wow.jpg",
+      date: "2019-05-29",
+      summary: "Yeah yeah, we want it.",
       content:
         "Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem"
     },
     3: {
-      title: "Sanctuary progressing in MC!",
-      img: "public/assets/moltencore.jpg",
-      content:
-        "Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem"
-    },
-    4: {
-      title: "Sanctuary progressing in MC!",
-      img: "public/assets/moltencore.jpg",
+      title: "Discord server online",
+      img: "public/assets/discord.jpg",
+      date: "2019-05-29",
+      summary: "Controlfreak just launched our Discord server.",
       content:
         "Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem"
     }
