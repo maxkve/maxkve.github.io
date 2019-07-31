9a8f90f3316b197e8ff42b4e29cce0ab0c34d116
Max Kvelland <max.kvelland@gmail.com>
2019-07-31 16:32:49 +0200

    Roster update + added pagination to news

--- public/components/layout/newsFeed/newsFeed.js
+++ public/components/layout/newsFeed/newsFeed.js
@@ -3,15 +3,47 @@
 angular.module("epgpApp").component("newsFeed", {
   bindings: {},
   templateUrl: "public/components/layout/newsFeed/newsFeed.html",
-  controller: function(newsService) {
+  controller: function(newsService, $filter) {
     let ctrl = this;
+    let news = [];
+    ctrl.activePageIndex = 0;
+    ctrl.numberOfPages = 0;
+    ctrl.newsViewModel = {};
+    ctrl.pagesViewModel = [];
+
+    function buildNewsViewModel(tag) {
+      let filteredNews = $filter("filter")(news, { tags: tag }).reverse();
+      ctrl.pagesViewModel = [];
+      ctrl.numberOfPages = Math.ceil(filteredNews.length / 6);
+      for (let i = 0; i < ctrl.numberOfPages; i++) {
+        let startIndex = i * 6;
+        let stopIndex = (i + 1) * 6;
+        ctrl.pagesViewModel.push(i + 1);
+        ctrl.newsViewModel[i] = filteredNews.slice(startIndex, stopIndex);
+      }
+      ctrl.pagesViewModel.reverse();
+    }
+
+    ctrl.olderNews = function() {
+      if (ctrl.activePageIndex != ctrl.numberOfPages - 1) {
+        ctrl.activePageIndex += 1;
+      }
+    };
+
+    ctrl.newerNews = function() {
+      if (ctrl.activePageIndex > 0) {
+        ctrl.activePageIndex -= 1;
+      }
+    };
 
     ctrl.selectTag = function(selectedTag) {
-      ctrl.selectedTag = selectedTag;
+      ctrl.activePageIndex = 0;
+      buildNewsViewModel(selectedTag);
     };
 
     ctrl.$onInit = function() {
-      ctrl.news = newsService.getNews();
+      news = newsService.getNewsReadOnly();
+      buildNewsViewModel("");
     };
 
     ctrl.$onChanges = function(changes) {};
--- public/components/services/news.service.js
+++ public/components/services/news.service.js
@@ -1,8 +1,8 @@
 "use strict";
 
 angular.module("epgpApp").service("newsService", function() {
-  let news = {
-    0: {
+  let news = [
+    {
       title: "We're recruiting for Classic!",
       img: "public/assets/classic.jpg",
       date: "2019-05-29",
@@ -11,7 +11,7 @@ angular.module("epgpApp").service("newsService", function() {
       content:
         "Standard Procedure is a PvE-oriented european guild looking for mature and friendly people for Classic. We're aiming for a semi-casual atmosphere and a stable and structured community. Please check out Organization above for information on how the guild is organized, Raids and Loot Rules for details on raiding, and Roster to see who's already on the team. Sounds good? Apply using our Google form and we'll be in touch!<br><br><a href='https://docs.google.com/forms/d/e/1FAIpQLSfqn8qNK1cxgCPWJ7cuB092Kj_JDqVgDYpMMHR8Bjy825pW0w/viewform' target='_blank'>Click here to apply!</a>"
     },
-    1: {
+    {
       title: "Player Introduction: Combobreaker",
       img: "public/assets/news/vector2.jpg",
       date: "2019-06-01",
@@ -20,7 +20,7 @@ angular.module("epgpApp").service("newsService", function() {
       content:
         "Hello, Combobreaker here, Guild Master (and Webmaster) of Standard Procedure. I'm a 28-year old UX-designer from Sweden who's been a WoW-player since late vanilla. I'm incredibly excited for Classic, very much looking forward to going back to Azeroth again. I'm currently not playing retail, although I did play up to...Legion, I think.<br><br>Back in Vanilla I used to play a gnome Mage named my first name + my surname, because apparently I wasn't big on the whole privacy thing back then. I didn't raid much at the time, just some MC and ZG, but the raids are something I'm looking forward to a lot this time around. Don't worry, I won't be leading you guys during raids, we have a bunch of very experienced raid leaders who will be taking care of that. Beyond that, I'm looking forward to enjoying the leveling experience again, the zones, the sense of community and the feeling of a connected world.<br><br>I can't wait to see you all in-game."
     },
-    2: {
+    {
       title: "Player Introduction: Zorn",
       img: "public/assets/news/zorn.png",
       date: "2019-06-04",
@@ -29,7 +29,7 @@ angular.module("epgpApp").service("newsService", function() {
       content:
         "Hello, I'm Zorn. Your Raid Leader and Off-tank by night, 31-year old Banker from Sweden by day. I've been an avid player of WoW since the original Beta back in 2004. I've played every expansion up to Legion (BFA being the first one I havent played on release).<br><br>I created my first character back in the Beta, a Human Warrior, and have had him as my main ever since. I raided quite hardcore in both Vanilla and TBC, reaching 4-Horsemen before the dreaded 2.0 patch tore our raid to pieces. I have a lot of experience as a Raid-Leader and Main Tank through all of Vanillas content, that being said I'm open to new ideas and tactics!<br><br>I'm really looking forward to experience all that Classic has to offer with all of you!"
     },
-    3: {
+    {
       title: "Zorn appears on Countdown to Classic",
       img: "public/assets/news/countdowntoclassic.jpg",
       date: "2019-07-03",
@@ -38,7 +38,7 @@ angular.module("epgpApp").service("newsService", function() {
       content:
         "Zorn recently appeared on episode #111 of Countdown to Classic where he spoke at length about Thunderfury, its state pre- and post-nerf, its role in tanking, if any other classes were eligible to receive it (sorry Hunters), and much more.<br><br>You can listen to the podcast on Josh's website here: <a href='https://countdowntoclassic.com/2019/07/03/episode-111-thunderfury-dr-lupo-on-wow-classic-streaming-a-listener-reply-to-world-buffs-eu-server-mayhem-the-making-of-wailing-caverns/' target='_blank'>Countdown to Classic</a>"
     },
-    4: {
+    {
       title: "Player Introduction: Ohhstar",
       img: "public/assets/news/ohhstar.png",
       date: "2019-07-21",
@@ -47,7 +47,7 @@ angular.module("epgpApp").service("newsService", function() {
       content:
         "Hey there! My name is Kajsa, I’m 29 years old and from Sweden. I work as a landlord in the weekdays and in the weekends I’m spending my time with my dog and my partner. Playing golf, board games, being in the nature and ofc being in front of the computer.<br><br>I have been playing world of Warcraft since the release of TBC. I’ve always been a big fan of raiding, and a bit of a farming geek. One of my main goals was to get all the mounts and pets that were in the game. My absolute favorite mounts were The Amani War Bear from ZA and Anzu from Sethekk Halls. When WotLK came, I fore sure were going to go for the Time Lost Protodrake!<br>I stayed in Stormpeaks, spamming the macro day and night, and after a while it came flying above me. So...of course I have “Time Lost” as well...<br><br>The last time I played on retail, I played up to legion then I felt that the game had changed too much and it wasn’t a game for me anymore. Not only for all the thousands of mounts and pets you could get that easily, but also for the community that had changed too much.<br>When I heard that classic was coming, there were no doubts that me and my friends were going to play. When I started in TBC my first character was a Druid named Ohhstar. I’ve had some alternative characters that I played on, and mage was always one of my favorite classes. So this time - I’ll be casting frostbolts!"
     },
-    5: {
+    {
       title: "Initial recruitment drive finished",
       img: "public/assets/news/recruit.jpg",
       date: "2019-07-21",
@@ -55,8 +55,28 @@ angular.module("epgpApp").service("newsService", function() {
       summary: "We've finished our initial general recruitment drive, but still need some specific roles.",
       content:
         "We're happy to announce that our initial general recruitment drive has finished. Thank you to those who applied, and welcome to those who were accepted. We're very excited about all the fine people we've managed to attract, and we're looking forward to getting to know you all. With that said, we're still looking for a couple of specific roles to top out the roster:<ul><li>1 Fury Warrior</li><li>2 Rogues</li><li>1 Holy Paladin</li><li>2 Mages</li></ul><a href='https://docs.google.com/forms/d/e/1FAIpQLSfqn8qNK1cxgCPWJ7cuB092Kj_JDqVgDYpMMHR8Bjy825pW0w/viewform' target='_blank'>Click here to apply!</a>"
+    },
+    {
+      title: "Player Introduction: Growgor",
+      img: "public/assets/news/growgor.jpg",
+      date: "2019-07-31",
+      tags: ["Player Introduction"],
+      summary: "Learn more about Growgor, 3D-artist by day and Rogue by night, in this short introductory piece.",
+      content:
+        "Hey everyone! My name is Nicolaj, I'm 32 years of age and I'm from central part Denmark, where I live together with my wife and our dog.<br><br>I work as a 3d artist in the architectural visualization industry, and have done so for over ten years now. When I'm not sitting in front of my computer and working or playing games, I'm usually found outside spending time on some of my countless hobbies, such as bow hunting, spear fishing, hiking, bushcrafting, or crafting stuff in my little workshop.<br><br>I've played World of Warcraft ever since the very first release day, and it has been my undisputed favorite game ever since. That said, after Burning Crusade and especially Wrath of the Lich King, it all went downhill from there, and shortly after Cataclysm I stopped playing the game. I did drop by in every expansion though, but only to be disappointed.<br><br>I'm very much looking forward to relive the glorious days of vanilla, and I'm excited to step in and clear especially Naxxramas, since I didn't get to clear that back in the days.<br><br>See you all in Azeroth!"
     }
-  };
+    /*
+    {
+      title: "Player Introduction: Froeen",
+      img: "public/assets/news/wow.jpg",
+      date: "2019-07-31",
+      tags: ["Player Introduction"],
+      summary: "Learn more about Froeen, boardgame enthusiast and future economic powerhouse, in this short introductory piece.",
+      content:
+        "Hey! I'm Froeen from Denmark. I am 26 years old and I work as a marketing specialist in a startup within music, which is a lot of fun. When I'm not eagerly waiting classic wow launch I play board games like Rising Sun, Time Stories, 7th Continent, Gloomhaven and Star Wars Imperial Assault as well as hanging out with my friends and wife or playing Crash Team Racing on my playstation.<br><br>I have played WoW since week 1 of TBC so unfortunately I'm not a vanilla player. My favorite expansion (so far) is TBC and I can't wait to get back to the game that I love. <br><br>I have raided in TBC, WOTLK, skipped Cata, raided again in MOP and skipped WoD and then played PvP in Legion. I was a raidleader in MOP where we were the best guild on our server's faction, and getting all the ahead of the curve achievements, which is my best memories with WoW. <br><br>I love the social aspect of WoW and I love making tons of gold as well, so if you need a loan you can come to me :-D"
+		}
+		*/
+  ];
 
   this.getNewsReadOnly = function() {
     return angular.copy(news);
