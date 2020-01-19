3a1e7f1a4859cee4ca764a6b5d10a06aec00c303
Max Kvelland <max.kvelland@gmail.com>
2020-01-19 22:46:50 +0100

    Raid update + polish

--- public/components/services/raid.service.js
+++ public/components/services/raid.service.js
@@ -3,28 +3,28 @@
 angular.module("epgpApp").service("raidService", function() {
   const raids = {
     moltencore: {
-      /* Phase 1 */
       title: "Molten Core",
       img: "moltencore3.jpg",
       killCount: "10 / 10",
+      phase: "Phase 1",
       bosses: {
         lucifron: { name: "Lucifron", killed: true, img: "lucifron.jpg" },
         magmadar: { name: "Magmadar", killed: true, img: "magmadar.jpg" },
         gehennas: { name: "Gehennas", killed: true, img: "gehennas.jpg" },
         garr: { name: "Garr", killed: true, img: "garr.jpg" },
         shazzrah: { name: "Shazzrah", killed: true, img: "shazzrah.jpg" },
-        barongeddon: { name: "Baron Geddon", killed: true, img: "barongeddon.jpg" },
-        sulfuronharbinger: { name: "Sulfuron Harbinger", killed: true, img: "sulfuronharbinger.jpg" },
-        golemaggtheincinerator: { name: "Golemagg", killed: true, img: "golemaggtheincinerator.jpg" },
-        majordomoexecutus: { name: "Majordomo", killed: true, img: "majordomoexecutus.jpg" },
+        barongeddon: { name: "Baron Geddon", killed: true, img: "geddon.jpg" },
+        sulfuronharbinger: { name: "Sulfuron Harbinger", killed: true, img: "sulfuron.jpg" },
+        golemaggtheincinerator: { name: "Golemagg", killed: true, img: "golemagg.jpg" },
+        majordomoexecutus: { name: "Majordomo", killed: true, img: "majordomo.jpg" },
         ragnaros: { name: "Ragnaros", killed: true, img: "ragnaros.jpg" }
       }
     },
     onyxia: {
-      /* Phase 1 */
       title: "Onyxia",
       img: "onyxia2.jpg",
       killCount: "1 / 1",
+      phase: "Phase 1",
       bosses: {
         onyxia: {
           name: "Onyxia",
@@ -32,41 +32,40 @@ angular.module("epgpApp").service("raidService", function() {
           img: "onyxia.jpg"
         }
       }
-    }
-    /*world: {
-      
+    },
+    world: {
       title: "World Bosses",
       img: "onyxia2.jpg",
+      killCount: "0 / 2",
+      phase: "Phase 2",
       bosses: {
         azuregos: {
           name: "Azuregos",
           killed: false,
-          img: "onyxia.jpg"
+          img: "azuregos.jpg"
         },
         kazzak: {
           name: "Kazzak",
           killed: false,
-          img: "onyxia.jpg"
+          img: "kazzak.jpg"
         }
       }
     },
     blackwinglair: {
-      
       title: "Blackwing Lair",
       img: "moltencore3.jpg",
+      killCount: "0 / 7",
+      phase: "Phase 3",
       bosses: {
-        lucifron: { name: "Lucifron", killed: false, img: "lucifron.jpg" },
-        magmadar: { name: "Magmadar", killed: false, img: "magmadar.jpg" },
-        gehennas: { name: "Gehennas", killed: false, img: "gehennas.jpg" },
-        garr: { name: "Garr", killed: false, img: "garr.jpg" },
-        shazzrah: { name: "Shazzrah", killed: false, img: "shazzrah.jpg" },
-        barongeddon: { name: "Baron Geddon", killed: false, img: "barongeddon.jpg" },
-        sulfuronharbinger: { name: "Sulfuron Harbinger", killed: false, img: "sulfuronharbinger.jpg" },
-        golemaggtheincinerator: { name: "Golemagg", killed: false, img: "golemaggtheincinerator.jpg" },
-        majordomoexecutus: { name: "Majordomo", killed: false, img: "majordomoexecutus.jpg" },
-        ragnaros: { name: "Ragnaros", killed: false, img: "ragnaros.jpg" }
+        vaelastrasz: { name: "Vaelastrasz", killed: false, img: "vaelastrasz.jpg" },
+        broodlord: { name: "Broodlord Lashlayer", killed: false, img: "broodlord.jpg" },
+        firemaw: { name: "Firemaw", killed: false, img: "firemaw.jpg" },
+        ebonroc: { name: "Ebonroc", killed: false, img: "ebonroc.jpg" },
+        flamegor: { name: "Flamegor", killed: false, img: "flamegor.jpg" },
+        chromaggus: { name: "Chromaggus", killed: false, img: "chromaggus.jpg" },
+        nefarian: { name: "Nefarian", killed: false, img: "nefarian.jpg" }
       }
-		}*/
+    }
   };
 
   this.getRaidsReadOnly = function() {
