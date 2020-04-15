8dda4c99d31131042edd05669ede3698bbfe90e6
Max Kvelland <max.kvelland@gmail.com>
2020-04-15 06:39:57 +0200

    ZG raid update

--- public/components/services/raid.service.js
+++ public/components/services/raid.service.js
@@ -1,10 +1,9 @@
 "use strict";
 
-angular.module("epgpApp").service("raidService", function() {
+angular.module("epgpApp").service("raidService", function () {
   const raids = {
     moltencore: {
       title: "Molten Core",
-      img: "moltencore3.jpg",
       killCount: "10 / 10",
       phase: "Phase 1",
       bosses: {
@@ -17,12 +16,11 @@ angular.module("epgpApp").service("raidService", function() {
         sulfuronharbinger: { name: "Sulfuron Harbinger", killed: true, killDate: "2019-09-30", img: "sulfuron.jpg" },
         golemaggtheincinerator: { name: "Golemagg", killed: true, killDate: "2019-09-30", img: "golemagg.jpg" },
         majordomoexecutus: { name: "Majordomo", killed: true, killDate: "2019-09-30", img: "majordomo.jpg" },
-        ragnaros: { name: "Ragnaros", killed: true, killDate: "2019-10-06", img: "ragnaros.jpg" }
-      }
+        ragnaros: { name: "Ragnaros", killed: true, killDate: "2019-10-06", img: "ragnaros.jpg" },
+      },
     },
     onyxia: {
       title: "Onyxia",
-      img: "onyxia2.jpg",
       killCount: "1 / 1",
       phase: "Phase 1",
       bosses: {
@@ -30,13 +28,12 @@ angular.module("epgpApp").service("raidService", function() {
           name: "Onyxia",
           killed: true,
           killDate: "2019-10-02",
-          img: "onyxia.jpg"
-        }
-      }
+          img: "onyxia.jpg",
+        },
+      },
     },
     world: {
       title: "World Bosses",
-      img: "onyxia2.jpg",
       killCount: "0 / 2",
       phase: "Phase 2",
       bosses: {
@@ -44,19 +41,18 @@ angular.module("epgpApp").service("raidService", function() {
           name: "Azuregos",
           killed: false,
           killDate: "",
-          img: "azuregos.jpg"
+          img: "azuregos.jpg",
         },
         kazzak: {
           name: "Kazzak",
           killed: false,
           killDate: "",
-          img: "kazzak.jpg"
-        }
-      }
+          img: "kazzak.jpg",
+        },
+      },
     },
     blackwinglair: {
       title: "Blackwing Lair",
-      img: "moltencore3.jpg",
       killCount: "8 / 8",
       phase: "Phase 3",
       bosses: {
@@ -67,16 +63,36 @@ angular.module("epgpApp").service("raidService", function() {
         ebonroc: { name: "Ebonroc", killed: true, killDate: "2020-02-13", img: "ebonroc.jpg" },
         flamegor: { name: "Flamegor", killed: true, killDate: "2020-02-13", img: "flamegor.jpg" },
         chromaggus: { name: "Chromaggus", killed: true, killDate: "2020-02-13", img: "chromaggus.jpg" },
-        nefarian: { name: "Nefarian", killed: true, killDate: "2020-02-13", img: "nefarian.jpg" }
-      }
-    }
+        nefarian: { name: "Nefarian", killed: true, killDate: "2020-02-13", img: "nefarian.jpg" },
+      },
+    },
+    zulgurub: {
+      title: "Zul'Gurub",
+      killCount: "0 / 13",
+      phase: "Phase 4",
+      bosses: {
+        jeklik: { name: "High Priestess Jeklik", killed: false, killDate: "2020-04-16", img: "jeklik.jpg" },
+        venoxis: { name: "High Priest Venoxis", killed: false, killDate: "2020-04-16", img: "venoxis.jpg" },
+        marli: { name: "High Priestess Mar'li", killed: false, killDate: "2020-04-16", img: "marli.jpg" },
+        thekal: { name: "High Priest Thekal", killed: false, killDate: "2020-04-16", img: "thekal.jpg" },
+        arlokk: { name: "High Priestess Arlokk", killed: false, killDate: "2020-04-16", img: "arlokk.jpg" },
+        mandokir: { name: "Bloodlord Mandokir", killed: false, killDate: "2020-04-16", img: "mandokir.jpg" },
+        hakkar: { name: "Hakkar the Soulflayer", killed: false, killDate: "2020-04-16", img: "hakkar.jpg" },
+        wushoolay: { name: "Wushoolay", killed: false, killDate: "2020-04-16", img: "wushoolay.jpg" },
+        renataki: { name: "Renataki", killed: false, killDate: "2020-04-16", img: "renataki.jpg" },
+        hazzarah: { name: "Hazza'rah", killed: false, killDate: "2020-04-16", img: "hazzarah.jpg" },
+        grilek: { name: "Gri'lek", killed: false, killDate: "2020-04-16", img: "grilek.jpg" },
+        jindo: { name: "Jin'do the Hexxer", killed: false, killDate: "2020-04-16", img: "jindo.jpg" },
+        gahzranka: { name: "Gahz'ranka", killed: false, killDate: "2020-04-16", img: "gahzranka.jpg" },
+      },
+    },
   };
 
-  this.getRaidsReadOnly = function() {
+  this.getRaidsReadOnly = function () {
     return angular.copy(raids);
   };
 
-  this.getRaids = function() {
+  this.getRaids = function () {
     return raids;
   };
 });
